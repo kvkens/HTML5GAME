3f5567eb5119d1def0813c864ca3196b6285a5f1
JayChou <kvkens@qq.com>
2016-02-23 18:08:17 +0800

    add

--- ttplayer/js/player.js
+++ ttplayer/js/player.js
@@ -1,8 +1,8 @@
 var Player = function(){
 	this.audio = document.querySelectorAll("#player");
 	this.songsList = document.querySelectorAll("#songsList");
-	this.lrcList = document.querySelectorAll("#lrcList")
-;}
+	this.lrcList = document.querySelectorAll("#lrcList");
+}
 Player.prototype = {
 	constructor : Player,
 	init : function(){
@@ -13,11 +13,24 @@ Player.prototype = {
 		xhttp.open("GET","./json/songslist.json",true);
 		xhttp.onreadystatechange = function(){
 			if(xhttp.readyState==4 && xhttp.status == 200){
-				var obj = (JSON.parse(xhttp.responseText));
-				alert(typeof obj);
+				var obj = (JSON.parse(xhttp.responseText).data);
+				ctx.loadSongsHTML(obj);
 			}
 		}
 		xhttp.send();
+	},
+	loadSongsHTML : function(data){
+		var documentframe = document.createDocumentFragment();
+		data.forEach(function(v){
+			var a = document.createElement("a"),
+				li = document.createElement("li");
+			a.href = "javascript:void(0)";
+			a.textContent = v.song + " - " + v.singer;
+			li.appendChild(a);
+			documentframe.appendChild(li);
+		});
+		this.songsList[0].appendChild(documentframe);
+		console.log(this.songsList);
 	}
 }
 window.onload = function(){
